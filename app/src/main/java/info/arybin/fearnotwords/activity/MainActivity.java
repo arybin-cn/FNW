aeb8f6f451240a4ef19ad6906d25b9205d90f3c2
AryBin <arybin@163.com>
2017-03-25 19:54:07 +0800

    refined import

--- app/src/main/java/info/arybin/fearnotwords/activity/MainActivity.java
+++ app/src/main/java/info/arybin/fearnotwords/activity/MainActivity.java
@@ -1,21 +1,15 @@
 package info.arybin.fearnotwords.activity;
 
-import android.content.EntityIterator;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.flaviofaria.kenburnsview.KenBurnsView;
 
-import org.litepal.crud.DataSupport;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import eightbitlab.com.blurview.BlurView;
 import info.arybin.fearnotwords.R;
-import info.arybin.fearnotwords.model.LocalizedEntity;
-import info.arybin.fearnotwords.model.orm.Entity;
 import info.arybin.fearnotwords.ui.anim.SimpleTransitionGenerator;
 import info.arybin.fearnotwords.ui.view.FABRevealLayout;
 import info.arybin.fearnotwords.ui.view.SlideLayout;
